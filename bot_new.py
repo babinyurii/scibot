a9de9c9615163ff10a0c3fbaf8e19efeb5b0c096
Yuriy Babin <babin.yurii@gmail.com>
2025-06-15 12:40:52 +0300

    study further

--- bot_new.py
+++ bot_new.py
@@ -1,38 +1,35 @@
-"""
-This is a echo bot.
-It echoes any incoming text messages.
-"""
 
+import asyncio
 import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from dotenv import load_dotenv
+import os
 
-from aiogram import Bot, Dispatcher, executor, types
+load_dotenv()
 
-API_TOKEN = '7845449395:AAEdU9ImZ18skHqzEX45Ghq6QtBmbRA_6Ww'
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token=os.getenv('BOT_TOKEN'))
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+@dp.message(Command("hijack"))
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+@dp.message(Command("reply"))
+async def cmd_reply(message: types.Message):
+    await message.reply('Это ответ с "ответом"')
 
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
 
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-
-    await message.answer(message.text)
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == "__main__":
+    asyncio.run(main())
