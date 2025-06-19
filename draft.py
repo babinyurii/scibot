d840f16495879b5c86207e102a7678d74b1ea75a
Yuriy Babin <babin.yurii@gmail.com>
2025-06-19 14:09:42 +0300

    add schedule draft

--- draft.py
+++ draft.py
@@ -8,6 +8,11 @@ from aiogram import F
 from dotenv import load_dotenv
 import os
 from db import engine, PubMedSearch
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+scheduler = AsyncIOScheduler()
+
+
 
 
 load_dotenv()
@@ -40,14 +45,38 @@ async def var_1(message: types.Message):
     add_query(int(message.from_user.id), message.text)
     print('done')
 
+    #######################
+    # here search in pubmed without schedule
+    ########################
+
+    # calling func to send your his query digest
+    await send_message_to_user(user_id=message.from_user.id)
+
 @dp.message(F.text.contains("ввести ключевые слова для поиска"))
 async def var_1(message: types.Message):
     await message.reply("введите ключевые слова", reply_markup=types.ReplyKeyboardRemove())
     print(message.from_user.id)
-   
+
+
+
+
+####################################################
+# this will be function to send digest
+async def send_message_to_user(user_id):
+    try:
+        await bot.send_message(chat_id=user_id, text='i send you message by your id')
+        print(f"Message sent to user {user_id}")
+    except Exception as e:
+        print(f"Error sending message to user {user_id}: {e}")
+#
+##########################################################################
+
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    shedule_jobs() # call func of sheduling
+    scheduler.start() # start here
     await dp.start_polling(bot)
 
 
@@ -57,6 +86,17 @@ def add_query(user, query):
         session.add(query_record)
         session.commit()
 
+@dp.message()
+async def send_mes_test(dp: Dispatcher):
+    await bot.send_message(chat_id=5497349882, text='message by shedule')
+
+
+def shedule_jobs():
+    scheduler.add_job(send_mes_test, 'interval', seconds=5, args=(dp,))    
+
+
 
 if __name__ == "__main__":
+    
+    
     asyncio.run(main())
