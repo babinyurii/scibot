2055d925d9ffcdf9c1cbdd69ccbc3591d5ac9049
Yuriy Babin <babin.yurii@gmail.com>
2025-07-09 16:52:58 +0300

    edit bot.py  and db.py

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
-
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from sqlalchemy.orm import Session
 from aiogram import F
 from dotenv import load_dotenv
 import os
-from db import engine, PubMedSearch
+from db import engine, PubMedSearch, get_query, add_query
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pubmed_search import search, fetch_details
 
@@ -25,46 +23,39 @@ async def cmd_start(message: types.Message):
     kb = [
         [
         types.KeyboardButton(text="ввести ключевые слова для поиска"),
-        types.KeyboardButton(text="вариант 2"),
+        # types.KeyboardButton(text="вариант 2"),
         ],
     ]
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb,  
                                         resize_keyboard=True,
-                                        input_field_placeholder="нажмите на кнопку ниже, чтоб выбрать вариант")
+                                        input_field_placeholder="нажмите на кнопку ниже")
     await message.answer("выберите вариант действий", reply_markup=keyboard, )
 
 
-# here we react to the text of buttons
+@dp.message(F.text.contains("ввести ключевые слова для поиска"))
+async def var_1(message: types.Message):
+    await message.reply("введите ключевые слова для поиска в PubMed. Не более 3 слов, разделенных запятой", 
+                        reply_markup=types.ReplyKeyboardRemove())
+
+
 @dp.message(F.text.contains(","))
 async def var_1(message: types.Message):
-    await message.reply("записано")
-    print(type(message.text))
     add_query(int(message.from_user.id), message.text)
-    print('done')
-
-    #######################
-    # here search in pubmed without schedule
-    ########################
-
-    # calling func to send your his query digest
+    await message.reply("записано")
     await send_message_to_user(user_id=message.from_user.id)
+    
+    """ it'll be the query by shedule
     q_words= get_query(message.from_user.id).split(',')
     results = search(q_words[0])
     id_list = results['IdList']
     papers = fetch_details(id_list)
     for i, paper in enumerate(papers['PubmedArticle']):
         print("{}) {}".format(i+1, paper['MedlineCitation']['Article']['ArticleTitle']))
-
-@dp.message(F.text.contains("ввести ключевые слова для поиска"))
-async def var_1(message: types.Message):
-    await message.reply("введите ключевые слова", reply_markup=types.ReplyKeyboardRemove())
-    print(message.from_user.id)
-
-
-
+    """
 
 ####################################################
 # this will be function to send digest
+# placeholder to see it works
 async def send_message_to_user(user_id):
     try:
         await bot.send_message(chat_id=user_id, text='i send you message by your id')
@@ -74,48 +65,21 @@ async def send_message_to_user(user_id):
 #
 ##########################################################################
 
+@dp.message()
+async def send_mes_test(dp: Dispatcher):
+    q_words = get_query() # new
+    await bot.send_message(chat_id=5497349882, text='message by shedule') # old
+    await bot.send_message(chat_id=5497349882, text=q_words)
+
+def shedule_jobs():
+    scheduler.add_job(send_mes_test, 'interval', seconds=10, args=(dp,))    
 
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     shedule_jobs() # call func of sheduling
     scheduler.start() # start here
-    
     await dp.start_polling(bot)
 
-
-
-
-
-
-@dp.message()
-async def send_mes_test(dp: Dispatcher):
-    q_words = get_query(message.from_user.id) # new
-    await bot.send_message(chat_id=5497349882, text='message by shedule') # old
-    await bot.send_message(chat_id=5497349882, text=q_words)
-
-
-def shedule_jobs():
-    scheduler.add_job(send_mes_test, 'interval', minutes=2, args=(dp,))    
-
-#############################
-# db methods
-############################
-def add_query(user, query):
-    with Session(engine) as session:
-        query_record = PubMedSearch(user_id=user, query_words=query)
-        session.add(query_record)
-        session.commit()
-
-def get_query(user):
-    with Session(engine) as session:
-        search_query = session.query(PubMedSearch).filter_by(user_id=user).first()
-        print(type(search_query))
-        print(dir(search_query))
-        return search_query.query_words
-
-
 if __name__ == "__main__":
-    
-    
     asyncio.run(main())
--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
- 
+from sqlalchemy.orm import Session
+
 engine = create_engine('sqlite:///pubmed_search.db', echo = True)
 Base = declarative_base()
  
@@ -20,3 +21,20 @@ class PubMedSearch(Base):
         self.query_words = query_words    
 
 Base.metadata.create_all(engine)
+
+
+#############################
+# db methods
+############################
+def add_query(user, query):
+    with Session(engine) as session:
+        query_record = PubMedSearch(user_id=user, query_words=query)
+        session.add(query_record)
+        session.commit()
+
+def get_query(user):
+    with Session(engine) as session:
+        search_query = session.query(PubMedSearch).filter_by(user_id=user).first()
+        print(type(search_query))
+        print(dir(search_query))
+        return search_query.query_words
