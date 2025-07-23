19d69b6a40f505d0ff7811a4cd50f0c2d668b99d
Yuriy Babin <babin.yurii@gmail.com>
2025-07-23 17:42:08 +0300

    add schedule funcs

--- bot.py
+++ bot.py
@@ -19,35 +19,59 @@ dp = Dispatcher(storage=MemoryStorage())
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 
 
-async def search_pubmed_on_schedule(dp: Dispatcher):
-    records = get_records_by_schedule_interval('mondays')
+
+async def search_pubmed_on_schedule(interval):
+    print('#' * 100)
+    print('interval from search function: ', interval)
+    records = get_records_by_schedule_interval(interval)
     print('*' * 100)
+    # is there need to check if records from db are empty
     for record in records:
         print('id: ', record.user_id)
         print('query_words: ', record.query_words)
         print('interval: ', record.schedule_interval)
-    articles = ''
-    print(record.query_words.split(','))
-    query_words = record.query_words.split(',')
-    query_words = query_words[0:2]
-    for query_word in query_words:
+        articles = ''
+        print(record.query_words.split(','))
+        query_words = record.query_words.split(',')
+        query_words = query_words[0:2]
+        for query_word in query_words:
+            
+            try:
+                print(query_word)
+                articles += get_articles(query_words=query_word)
+                #articles = get_articles(query_words=query_word)
+            except Exception as e:
+                print ('error: ' * 100, e)
+                pass
+                # into log
         
-        try:
-            print(query_word)
-            articles += get_articles(query_words=query_word)
-            #articles = get_articles(query_words=query_word)
-        except Exception:
-            pass
-            # into log
-    
-    # TODO: add check somewhere for message len: 4096 utf chars
-    await bot.send_message(chat_id=record.user_id, text=articles)
+        # TODO: add check somewhere for message len: 4096 utf chars
+        await bot.send_message(chat_id=record.user_id, text=articles)
+
+
+async def search_pubmed_last_fri(dp: Dispatcher,):
+    await search_pubmed_on_schedule(interval='last_friday')
+
+async def search_pubmed_mon(dp: Dispatcher,):
+    await search_pubmed_on_schedule(interval='mondays')
+
+async def search_pubmed_fri(dp: Dispatcher,):
+    await search_pubmed_on_schedule(interval='fridays')
 
 
 def shedule_jobs():
-    #scheduler.add_job(search_pubmed_on_schedule,'interval', seconds=10, args=(dp, ))    
-    # here will be the call of the same function, but with different intervals
-    pass
+    #scheduler.add_job(search_pubmed_on_schedule, 'interval', seconds=15, args=(dp, ))    
+    
+    #scheduler.add_job(search_pubmed_last_fri, "cron", day="last fri", args=(dp, ))
+    #scheduler.add_job(search_pubmed_mon, 'cron', day_of_week='mon', hour=12, args=(dp, ) )
+    #scheduler.add_job(search_pubmed_fri, 'cron', day_of_week='fri', hour=12,  args=(dp, ))
+    
+    # test funcs
+    scheduler.add_job(search_pubmed_last_fri, 'interval', seconds=30,  args=(dp, ))
+    scheduler.add_job(search_pubmed_mon, 'interval', minutes=5, args=(dp, ) )
+    scheduler.add_job(search_pubmed_fri, 'interval', minutes=15,   args=(dp, ))
+
+    
 
 async def main():
     
